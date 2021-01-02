9bec3d7a8688d74be805037888cbd3d92a3533ad
Maksim Zakharov <max89701@gmail.com>
2021-01-02 15:49:55 +0300

    add auto-zip after build

---
+++ zip-dist.js
@@ -0,0 +1,27 @@
+var file_system = require('fs');
+var archiver = require('archiver');
+
+let rawdata = file_system.readFileSync('src/manifest.json')
+let manifest = JSON.parse(rawdata);
+
+var output = file_system.createWriteStream(`tiktok-chrome-extension-${manifest.version}.zip`);
+var archive = archiver('zip');
+
+output.on('close', function () {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('archiver has been finalized and the output file descriptor has closed.');
+});
+
+archive.on('error', function(err){
+    throw err;
+});
+
+archive.pipe(output);
+
+// append files from a sub-directory, putting its contents at the root of archive
+archive.directory('dist/', false);
+
+// // append files from a sub-directory and naming it `new-subdir` within the archive
+// archive.directory('subdir/', 'new-subdir');
+
+archive.finalize();
