0f1aa561cab3d0a88e42ca06fd0ce2fa153d1ecb
Maksim Zakharov <max89701@gmail.com>
2021-01-06 19:03:07 +0300

    Пока стырил загрузчик видео, позже переделать нужно

---
+++ src/background/download-video.js
@@ -0,0 +1,86 @@
+// background.js
+const download = {
+    async onDownload(tiktokURL) {
+        const formData = new FormData();
+        formData.append('url', tiktokURL);
+        const options = {
+            method: 'POST',
+            body: formData,
+            credentials: 'include',
+        };
+        const response = await fetch(`https://snaptik.app/action.php`, options);
+        const text = await response.text();
+
+        if (text.indexOf('error_token') !== -1) {
+            console.error('РќРµС‚ СЋР·РµСЂ С‚РѕРєРµРЅР°, РєСѓРєРё');
+            return false
+        }
+
+        return text.match(/<a .*n><\/a>/g).filter((item)=>{
+            return item.indexOf('.tiktokcdn') !== -1 || item.indexOf('.akamaized') !== -1
+        })[0].match(/href='.*'/)[0].replace(/href='/, '').replace(/'$/, '')
+    },
+    async onGetUser() {
+        const formData = new FormData();
+        const options = {
+            method: 'POST',
+            body: formData,
+            credentials: 'include',
+        };
+        const response = await fetch(`https://snaptik.app/check_token.php`, options);
+        const json = await response.json();
+
+        if (json && json.status && json.status === '100') {
+            console.log('РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ РїРѕР»СѓС‡РµРЅ')
+            return true;
+        } else {
+            console.log('РћС€РёР±РєР° РїРѕР»СѓС‡РµРЅРёСЏ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ')
+            return false;
+        }
+    },
+    async onController(tiktokURL, errorCounter = 0) {
+        let result = false;
+        let ok = await download.onGetUser();
+        ok && (
+            result = await download.onDownload(tiktokURL)
+        )
+        if (result) {
+            chrome.tabs.sendMessage(tabId, {
+                'cmd': 'onDownloadLink',
+                links: {
+                    outer: result,
+                    inner: tiktokURL,
+                },
+                errorCounter,
+            }); //РћС‚РґР°С‚СЊ РѕР±СЂР°С‚РЅРѕ
+        } else {
+
+        }
+    }
+}
+
+
+let tabId;
+
+chrome.runtime.onMessage.addListener( function({ cmd , data}, sender, sendResponse) {
+    chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
+        if (sender.url.indexOf('chrome-extension:') === -1) {
+            if (tabs.length < 1) {
+                console.error("no select tab");
+                chrome.runtime.sendMessage({cmd: "popup_error", result: {"status": -1, "msg": "Please Select One Tab."}});
+                return;
+            }
+            let current_tab = tabs[0];
+            let tab_url = current_tab['url'];
+            let tab_title = current_tab['title'];
+            tabId = current_tab.id;
+            console.log('tabId', tabId);
+        }
+
+        switch (cmd) {
+            case "download":
+                download.onController(data.tiktokURL, data.errorCounter)
+                break;
+        }
+    });
+});
--- src/content/content.js
+++ src/content/content.js
@@ -801,3 +801,186 @@ async function analyzeProfile() {
     document.querySelector('[data_content_start_analyzing]').removeAttribute('disabled');
     document.querySelector('[data_content_start_analyzing]').innerHTML = chrome.i18n.getMessage('content_start_analyzing');
 }
+
+// content-script.js
+let SELECTOR = {
+    buttons: {
+        dw: '.ext-dw-button',
+    },
+    feed: {
+        listens: [
+            '.lazyload-wrapper',
+            '.tt-feed',
+            '.video-feed-item',
+        ],
+        appendTo: '.default',
+    },
+    card: {
+        listens: [
+            '.video-card-big',
+            '.video-card-browse',
+            '.video-player',
+        ],
+        appendTo: '.video-card-container',
+    }
+}
+
+SELECTOR.listens = [
+    ...SELECTOR.feed.listens,
+    ...SELECTOR.card.listens,
+]
+
+SELECTOR.appendTo = [
+    SELECTOR.feed.appendTo,
+    SELECTOR.card.appendTo,
+]
+
+let $dwButton = document.createElement('div')
+
+$dwButton.className = SELECTOR.buttons.dw.substr(1);
+$dwButton.innerHTML = `
+    <div class="ext-lds-circle"><div></div></div>
+        <svg viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M25 50C38.8071 50 50 38.8071 50 25C50 11.1929 38.8071 0 25 0C11.1929 0 0 11.1929 0 25C0 38.8071 11.1929 50 25 50Z" fill="#FE2C55" fill-opacity="0.6"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M34 36L16 36L16 33L34 33L34 36ZM25 30L17.7397 21.8322C17.4531 21.5097 17.682 21 18.1134 21L23 21L23 15L27 15L27 21L31.8866 21C32.318 21 32.5469 21.5097 32.2603 21.8322L25 30Z" fill="white"/>
+    </svg>
+`;
+
+function loading(state = false) {
+    document.querySelectorAll(SELECTOR.buttons.dw).forEach(($btn)=>{
+        state ? $btn.classList.add('loading') : $btn.classList.remove('loading');
+    });
+}
+
+const view = {
+    init() {
+        document.querySelectorAll( SELECTOR.listens ).forEach(($card) => {
+            ($card.querySelectorAll( SELECTOR.appendTo ) || $card.closest( SELECTOR.appendTo )).forEach(($container) => {
+                view.addButton($container);
+                view.addTimeline($container);
+            })
+        })
+    },
+    addTimeline($container) {
+        SELECTOR.appendTo.forEach((selector) => {
+            let $video = $container.querySelector('video') || $container.closest('video')
+            if ($video && !$video.inited) {
+                $video.timer && clearTimeout($video.timer);
+                $video.timer = setTimeout( () => {
+                    let $oldVideoTimeline = $container.querySelector('.ext-timeline');
+                    $oldVideoTimeline && $oldVideoTimeline.remove();
+                    let $videoTimeline = document.createElement('div')
+                    $videoTimeline.className = 'ext-timeline';
+                    $container.appendChild($videoTimeline)
+                    $video.ontimeupdate = (event) => {
+                        let duration = $video.duration;
+                        let step = duration / 100;
+                        let asd = Math.abs((duration - event.currentTarget.currentTime) / step - 100) / 100;
+                        $videoTimeline.style.transform = `scaleX(${asd})`
+                    };
+                    $video.inited = true;
+                }, 1000)
+            }
+        })
+    },
+    addButton($container) {
+        $container.timer && clearTimeout($container.timer);
+        $container.timer = setTimeout( () => {
+            if (!$container.querySelector(SELECTOR.buttons.dw)) {
+                const $cloneButton  = $dwButton.cloneNode(true)
+                $cloneButton.onclick = async (event) => {
+
+                    if (event.currentTarget) {
+                        const $containerLink = event.currentTarget.parentElement.closest('.item-video-card-wrapper');
+                        const tiktokURL = $containerLink ? $containerLink.href : location.href
+
+                        loading(true)
+                        chrome.runtime.sendMessage({
+                            cmd: 'download',
+                            data: {
+                                tiktokURL
+                            }
+                        });
+                    } else {
+                        console.log('Цели клика нет клика.')
+                    }
+                }
+
+                $container.appendChild($cloneButton)
+            }
+        }, 10)
+    }
+}
+
+view.init();
+
+console.log('ttk')
+
+async function onDownloadLink(links, name = 'tiktok video', errorCounter = 0) {
+    let a = document.createElement("a");
+
+    try {
+        const response = await fetch(links.outer);
+        const blob = await response.blob();
+        a.download = name;
+        a.href = URL.createObjectURL(blob);
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        loading(false);
+    } catch (e) {
+        console.log(`Ошибка при загрузке самого видео файла номер: ${errorCounter}`);
+        if (errorCounter <= 1) {
+            chrome.runtime.sendMessage({
+                cmd: 'download',
+                data: {
+                    tiktokURL: links.inner,
+                    errorCounter: errorCounter + 1
+                }
+            });
+
+        } else {
+            loading(false);
+            console.log(`Ошибка финальный сбой при загрузке видео файла`);
+        }
+    }
+
+    // delete a;
+}
+
+chrome.storage.onChanged.addListener(async (event) => {
+    console.log('location.hostname', location.hostname);
+    if (location.hostname === "snaptik.app" && event.downloadIn && event.downloadIn.newValue) {
+        await download.onController(event.downloadIn.newValue.url);
+    }
+});
+
+let observer = new MutationObserver(mutations => {
+    for(let mutation of mutations) {
+        for(let node of mutation.addedNodes) {
+            if (!(node instanceof HTMLElement)) continue;
+
+            SELECTOR.listens.forEach((select)=>{
+                // console.log('mutations', select)
+                if (node.classList.contains(select.substr(1))) {
+                    view.init();
+                }
+            })
+        }
+    }
+});
+
+let $listenedContainer = document.querySelector('body');
+$listenedContainer && observer.observe($listenedContainer, {childList: true, subtree: true});
+
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+    switch (request.cmd) {
+        case 'onDownloadLink':
+            if (request.links.inner) {
+                await onDownloadLink(request.links)
+            } else {
+                console.log('Не получил ссылку перез скачиванием')
+            }
+            break;
+    }
+});
